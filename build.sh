7b48f9ecf8d786fb5c35eecb0b7938292327cb2d
beginor <beginor@qq.com>
2019-04-12 13:11:09 +0800

    6.1.8

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-docker build --rm --no-cache -t beginor/jexus-x64:6.0.0.18 .
+#!/bin/sh -e
+docker build --rm --no-cache -t beginor/jexus-x64:6.1.8 .
--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
-### every exit != 0 fails the script
-set -e
+#!/bin/sh -e
 apt-get update && apt-get upgrade -y
 # install wget to download jexus, curl for healthcheck.
-apt-get install -y curl procps
-curl --location --output jexus-6.0.0-x64.tar.gz https://linuxdot.net/down/jexus-6.0.0-x64.tar.gz
-tar -zxf jexus-6.0.0-x64.tar.gz && rm jexus-6.0.0-x64.tar.gz
+apt-get install -y --no-install-recommends curl ca-certificates procps
+curl --location --output jexus-6.1-x64.tar.gz https://linuxdot.net/down/jexus-6.1-x64.tar.gz
+tar -zxf jexus-6.1-x64.tar.gz && rm jexus-6.1-x64.tar.gz
 mv jexus /usr/ && cd /usr/jexus && /usr/jexus/jws regsvr
+apt-get remove -y ca-certificates
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
